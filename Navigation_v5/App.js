70b53bb9d950e411579a4ec5f87586e2c9e1bfdb
Anantha Madhava <ananthamadhava95@gmail.com>
2020-03-15 14:52:09 +0530

    React Navigation v5 - Stack, Tabs, Drawer, Authentication

---
+++ Navigation_v5/App.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import { AuthContext } from './src/context';
+
+import { SignIn, CreateAccount, Profile, Home, Search, Details, Search2, Splash } from './src/Screens';
+
+const AuthStack = createStackNavigator();
+const AuthStackScreen = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen 
+      name="SignIn" 
+      component={SignIn} 
+      options={{ title: 'Sign In' }} 
+    />
+    <AuthStack.Screen 
+      name="CreateAccount" 
+      component={CreateAccount} 
+      options={{ title: 'Create Account' }} 
+    />
+  </AuthStack.Navigator>
+)
+
+const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name="Home" component={Home} />
+    <HomeStack.Screen 
+      name="Details" 
+      component={Details} 
+      options={({route}) => ({title: route.params.name})}
+    />
+  </HomeStack.Navigator>
+)
+
+const SearchStackScreen = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen name="Search" component={Search} />
+    <SearchStack.Screen name="Search2" component={Search2} />
+  </SearchStack.Navigator>
+);
+
+const ProfileStack = createStackNavigator();
+const ProfileStackScreen = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen name="Profile" component={Profile} />
+  </ProfileStack.Navigator>
+)
+
+const TabsScreen = () => (
+  <Tabs.Navigator>
+    <Tabs.Screen 
+      name="Home" 
+      component={HomeStackScreen} 
+    />
+    <Tabs.Screen 
+      name="Search" 
+      component={SearchStackScreen}
+    />
+  </Tabs.Navigator>
+)
+
+const Drawer = createDrawerNavigator();
+const DrawerScreen = () => (
+  <Drawer.Navigator initialRouteName="Profile">
+    <Drawer.Screen name="Home" component={TabsScreen} />
+    <Drawer.Screen name="Profile" component={ProfileStackScreen} />
+  </Drawer.Navigator>
+)
+
+const RootStack = createStackNavigator();
+const RootStackScreen = ({ useToken }) => (
+  <RootStack.Navigator headerMode="none">
+    {useToken ? (
+      <RootStack.Screen 
+        name="App" 
+        component={DrawerScreen} 
+        options={{ animationEnabled: false }}
+      />
+    ) : (
+      <RootStack.Screen 
+        name="Auth" 
+        component={AuthStackScreen} 
+        options={{ animationEnabled: false }}
+      />
+    )}
+  </RootStack.Navigator>
+)
+
+
+export default () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [useToken, setuserToken] = React.useState(null);
+
+  const authContext = React.useMemo(() => {
+    return {
+      signIn: () => {
+        setIsLoading(false);
+        setuserToken('asdf');
+      },
+      signUp: () => {
+        setIsLoading(false);
+        setuserToken('asdf');
+      },
+      signOut: () => {
+        setIsLoading(false);
+        setuserToken(null);
+      }
+    }
+  }, [])
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }, [])
+
+  if(isLoading) {
+    return <Splash />
+  }
+  return (
+    <AuthContext.Provider value={authContext} >
+      <NavigationContainer>
+        <RootStackScreen useToken={useToken} />
+      </NavigationContainer>
+    </AuthContext.Provider>
+  )
+}
